04e162837d06652146ef767a25109905288d8b98
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2020-10-01 13:52:30 +0200

    Merged Master into Dev to start working

---
+++ Phoebus_Engine/Application.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "p2List.h"
+#include "Globals.h"
+#include "Timer.h"
+#include "Module.h"
+#include "ModuleWindow.h"
+#include "ModuleInput.h"
+#include "ModuleAudio.h"
+#include "ModuleRenderer3D.h"
+#include "ModuleRenderer2D.h"
+
+class Application
+{
+public:
+	ModuleWindow* window;
+	ModuleInput* input;
+	ModuleAudio* audio;
+	ModuleRenderer3D* renderer3D;
+	ModuleRenderer2D* renderer2D;
+	bool debug;
+	bool renderPrimitives;
+private:
+
+	Timer	ms_timer;
+	float	dt;
+	p2List<Module*> list_modules;
+
+public:
+
+	Application();
+	~Application();
+
+	bool Init();
+	update_status Update();
+	bool CleanUp();
+
+private:
+
+	void AddModule(Module* mod);
+	void PrepareUpdate();
+	void FinishUpdate();
+};
