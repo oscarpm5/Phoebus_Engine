939f173ef960441d63c5e0a1d900895892d681df
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2020-10-18 13:23:07 +0200

    0.0.7.1 - Vertex normals added
    -A new variable has been added to the mesh class which let you choose between different normal display modes
    -Made some of the Mesh methods private
    -Vertex normals implemented

--- Phoebus_Engine/Mesh.h
+++ Phoebus_Engine/Mesh.h
@@ -6,24 +6,34 @@ enum class MeshDrawMode
 	DRAW_MODE_WIRE,
 	DRAW_MODE_BOTH
 };
+enum class NormalDrawMode
+{
+	NORMAL_MODE_VERTEX,
+	NORMAL_MODE_FACES,
+	NORMAL_MODE_BOTH,
+	NORMAL_MODE_NONE
+};
 
 class Mesh
 {
 public:
 
-	Mesh(std::vector<float> vertices, std::vector<unsigned int> indices,std::vector<float> normals);
+	Mesh(std::vector<float> vertices, std::vector<unsigned int> indices, std::vector<float> normals);
 	Mesh(const Mesh& other);
 	~Mesh();
 
 	void Draw();
+
+private:
 	void GenerateBuffers();
 	void FreeBuffers();
-
-
+	void DrawVertexNormals();
+	void DrawFacesNormals();
 
 public:
 
 	MeshDrawMode drawMode;
+	NormalDrawMode normalMode;
 
 	unsigned int idIndex; // index in VRAM
 	std::vector<unsigned int> indices;//index array
