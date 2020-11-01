9b468874db9c962ac6bf071dd1961edea5e16c63
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2020-11-01 11:38:39 +0100

    0.1.0.17 - All hierarchy objects can now be selected

--- Phoebus_Engine/Hierarchy.cpp
+++ Phoebus_Engine/Hierarchy.cpp
@@ -38,43 +38,47 @@ void SeekMyChildren(GameObject* myself)
 
 		ImGui::PushStyleColor(ImGuiCol_Text, ChooseMyColor(myself));
 
-		if (ImGui::TreeNodeEx(myself->GetName().c_str(), TreeNodeEx_flags))
-		{
-			if (ImGui::IsItemClicked())
-			{
-				if (!App->editor3d->GetSelectedGameObject().empty()) 
-				{
-					App->editor3d->GetSelectedGameObject().back()->focused = false;
-				}
-				App->editor3d->SetSelectedGameObject(myself);
-				App->editor3d->GetSelectedGameObject().back()->focused = true;
+		bool open = ImGui::TreeNodeEx(myself->GetName().c_str(), TreeNodeEx_flags);
 
+		if (ImGui::IsItemClicked())
+		{
+			if (!App->editor3d->GetSelectedGameObject().empty())
+			{
+				App->editor3d->GetSelectedGameObject().back()->focused = false;
 			}
+			App->editor3d->SetSelectedGameObject(myself);
+			App->editor3d->GetSelectedGameObject().back()->focused = true;
+
+		}
+
+		if (open == true)
+		{
 
 			for (int i = 0; i < myself->children.size(); i++)
 			{
 				SeekMyChildren(myself->children[i]);
 			}
-
-			//if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None))
-			//{
-			//	//We are carrying the pointer to the Game Object as our payload
-			//	ImGui::SetDragDropPayload("PointerToGameObject", myself, sizeof(GameObject*)); //We know any pointer is 4 bytes, but this way of putting it makes it clearer to understand what are we doing
-			//	ImGui::EndDragDropSource();
-			//}
-
-			//if (ImGui::BeginDragDropTarget())
-			//{
-			//	if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("PointerToGameObject"))
-			//	{
-			//		IM_ASSERT(payload->DataSize == sizeof(GameObject*)); //just making sure we received what we're looking for
-			//		//myself->children.push_back((GameObject*)payload->Data);//cast required since payload is void*. This makes Adri scared
-			//	}
-			//	ImGui::EndDragDropTarget();
-			//}
-
 			ImGui::TreePop();
 		}
+
+		//if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None))
+		//{
+		//	//We are carrying the pointer to the Game Object as our payload
+		//	ImGui::SetDragDropPayload("PointerToGameObject", myself, sizeof(GameObject*)); //We know any pointer is 4 bytes, but this way of putting it makes it clearer to understand what are we doing
+		//	ImGui::EndDragDropSource();
+		//}
+
+		//if (ImGui::BeginDragDropTarget())
+		//{
+		//	if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("PointerToGameObject"))
+		//	{
+		//		IM_ASSERT(payload->DataSize == sizeof(GameObject*)); //just making sure we received what we're looking for
+		//		//myself->children.push_back((GameObject*)payload->Data);//cast required since payload is void*. This makes Adri scared
+		//	}
+		//	ImGui::EndDragDropTarget();
+		//}
+
+
 		ImGui::PopStyleColor();
 	}
 }
