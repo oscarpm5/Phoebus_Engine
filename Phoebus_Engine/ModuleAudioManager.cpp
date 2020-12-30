fa622ee7a8528df7c6a567d80e2c0c891f8096c1
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2020-12-30 16:04:57 +0100

    0.7.1.0 - FOR GOD'S SAKE it compiles
    Updates:
    - Started implementing Wwise AGAIN, it compiles for now

--- Phoebus_Engine/ModuleAudioManager.cpp
+++ Phoebus_Engine/ModuleAudioManager.cpp
@@ -1,6 +1,8 @@
 #include "ModuleAudioManager.h"
+#include <assert.h>
 #include "wwise_libraries.h"
-#include "Wwise/include/Win32/AkFilePackageLowLevelIOBlocking.h"
+#include "Wwise/low_level_IO/Win32/AkFilePackageLowLevelIOBlocking.h"
+//#include "AK/DefaultIO/Win32/AkFilePackageLowLevelIOBlocking.h"
 
 CAkFilePackageLowLevelIOBlocking g_lowLevelIO;
 
@@ -13,61 +15,19 @@ ModuleAudioManager::~ModuleAudioManager()
 }
 
 bool ModuleAudioManager::Init()
-{
+{/*
 	AkMemSettings memSettings;
 
-	memSettings.uMaxNumPools = 20;
+	AK::MemoryMgr::GetDefaultSettings(memSettings);
 
 	if (AK::MemoryMgr::Init(&memSettings) != AK_Success)
+	{
 
-	{
-		LOG("[error]Could not create the memory manager.");
+		assert(!"Could not create the memory manager.");
 
 		return false;
 
-	}
-	AkStreamMgrSettings stmSettings;
-	AK::StreamMgr::GetDefaultSettings(stmSettings);
-	// Customize the Stream Manager settings here.
-	if (!AK::StreamMgr::Create(stmSettings))
-	{
-		LOG("[error]Could not create the Streaming Manager");
-		return false;
-	}
-	AkDeviceSettings deviceSettings;
-	AK::StreamMgr::GetDefaultDeviceSettings(deviceSettings);
-	if (g_lowLevelIO.Init(deviceSettings) != AK_Success)
-	{
-		LOG("[error]Could not create the streaming device and Low-Level I/O system");
-		return false;
-	}
-
-	AkInitSettings initSettings;
-	AkPlatformInitSettings platformInitSettings;
-	AK::SoundEngine::GetDefaultInitSettings(initSettings);
-	AK::SoundEngine::GetDefaultPlatformInitSettings(platformInitSettings);
-	if (AK::SoundEngine::Init(&initSettings, &platformInitSettings) != AK_Success)
-	{
-		LOG("[error]Could not initialize the Sound Engine.");
-		return false;
-	}
-	AkMusicSettings musicInit;
-	AK::MusicEngine::GetDefaultInitSettings(musicInit);
-	if (AK::MusicEngine::Init(&musicInit) != AK_Success)
-	{
-		LOG("[error]Could not initialize the Music Engine.");
-		return false;
-	}
-#ifndef AK_OPTIMIZED
-	AkCommSettings commSettings;
-	AK::Comm::GetDefaultInitSettings(commSettings);
-	if (AK::Comm::Init(commSettings) != AK_Success)
-	{
-		LOG("[error]Could not initialize communication.");
-		return false;
-	}
-#endif // AK_OPTIMIZED
-
+	}*/
 
 	return true;
 }
